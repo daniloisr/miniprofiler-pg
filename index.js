2054d0440a99ede5275e8b9ebbd0598b27d43e98
Danilo Resende <danilo.resende@toptal.com>
2018-07-26 16:22:41 -0300

    Handle disabled miniprofiler for async contexts

--- index.js
+++ index.js
@@ -33,22 +33,25 @@ const asyncCtx = new AsyncContext();
 let pgQuery;
 
 module.exports = function(pg) {
-  pgQuery = pgQuery || pg.Client.prototype.query;
-
   return {
     name: 'pg',
     handler: function(req, _res, next) {
       asyncCtx.set(req);
-      if (!asyncCtx.miniprofiler || !asyncCtx.miniprofiler.enabled || pg.Client.prototype.query !== pgQuery)
-        return next();
 
+      // Install this provider only once
+      if (pgQuery) return next();
+
+      pgQuery = pgQuery || pg.Client.prototype.query;
       pg.Client.prototype.query = function(config, values, callback) {
+        if (!asyncCtx.miniprofiler || !asyncCtx.miniprofiler.enabled || pg.Client.prototype.query !== pgQuery)
+          return pgQuery.call(this, ...arguments);
+
         if (callback) {
           asyncCtx.miniprofiler.timeQuery('sql', config.toString(), pgQuery.bind(this), config, values, callback);
         } else {
           const timing = asyncCtx.miniprofiler.startTimeQuery('sql', config.toString());
           return pgQuery
-            .call(this, config, values, callback)
+            .call(this, ...arguments)
             .then((res) => {
               asyncCtx.miniprofiler.stopTimeQuery(timing);
               return res;
